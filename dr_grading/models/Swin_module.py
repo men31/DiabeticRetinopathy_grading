0212425ab49fd95111388e53c91df2e18448e74c
men31 <R.Yodsungnoen@gmail.com>
2025-04-17 01:06:53 +0700

    Update Swin_module.py

--- dr_grading/models/Swin_module.py
+++ dr_grading/models/Swin_module.py
@@ -34,6 +34,49 @@ class Swin_S(nn.Module):
         return output
     
 
+class Swin_V2_S(nn.Module):
+    def __init__(
+        self, num_classes: int, transfer: bool = True, activation: str = "relu", 
+        in_channels:int=3, encoder_model:Literal['simple', 'unet']="unet",
+    ):
+        super(Swin_V2_S, self).__init__()
+        self.transfer = transfer
+        self.num_classes = num_classes
+        self.activation = activation
+        self.in_channels = in_channels
+        self.encoder_model = encoder_model
+        self.hidden_layers = [256, 128, 128, 64]
+        self.weight = models.Swin_V2_S_Weights.IMAGENET1K_V1
+        self.model = self.__create_architecture()
+    
+    def __create_architecture(self):
+        model_lst = []
+        if self.in_channels != 3:
+            if self.encoder_model == "simple":
+                enc_rgb = ConvEncoder(in_channels=self.in_channels, out_channels=3)
+            elif self.encoder_model == "unet":
+                enc_rgb = UNet(in_channels=self.in_channels, out_channels=3, features=(64, 128), dropout=0.3)
+            else:
+                raise ValueError(f"[-] Not found encoder model: {self.encoder_model}")
+            model_lst.append(enc_rgb)
+
+        model = models.swin_v2_s(weights=self.weight)
+        if self.transfer:
+            for param in model.parameters():
+                param.requires_grad = False
+        model.head = Identity()
+        model_lst.append(model)
+
+        head = ClassifierHead(768, self.num_classes, self.hidden_layers, activation=self.activation)
+        model_lst.append(head)
+
+        return nn.Sequential(*model_lst)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        output = self.model(x)
+        return output
+    
+
 class Swin_V2_B(nn.Module):
     def __init__(
         self, num_classes: int, transfer: bool = True, activation: str = "relu", 
